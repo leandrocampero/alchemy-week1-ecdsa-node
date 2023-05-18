8049948bf6ea75b3792abd3988a116e4a6b78902
Leandro Campero <leandrocampero14@gmail.com>
2023-05-17 22:26:57 -0300

    generate signature and send it to server

--- client/src/Transfer.jsx
+++ client/src/Transfer.jsx
@@ -1,7 +1,15 @@
 import { useState } from "react";
 import server from "./server";
+import { secp256k1 } from "ethereum-cryptography/secp256k1.js";
+import { keccak256 } from "ethereum-cryptography/keccak.js";
+import { toHex, utf8ToBytes } from "ethereum-cryptography/utils";
 
-function Transfer({ address, setBalance }) {
+/**
+ * In order to secure a trasnfer, I should send along with the publicKey, recipient and amount,
+ * a hashed message (the string of the transfer data object) and the signed message
+ * and the server should verify the transaccion with the data received
+ */
+function Transfer({ address, setBalance, privateKey }) {
   const [sendAmount, setSendAmount] = useState("");
   const [recipient, setRecipient] = useState("");
 
@@ -10,6 +18,16 @@ function Transfer({ address, setBalance }) {
   async function transfer(evt) {
     evt.preventDefault();
 
+    const txData = {
+      sender: address,
+      amount: parseInt(sendAmount),
+      recipient: recipient,
+    };
+
+    const hash = toHex(keccak256(utf8ToBytes(JSON.stringify(txData))));
+
+    const signature = secp256k1.sign(hash, privateKey);
+
     try {
       const {
         data: { balance },
@@ -17,9 +35,16 @@ function Transfer({ address, setBalance }) {
         sender: address,
         amount: parseInt(sendAmount),
         recipient,
+        hash,
+        signature: {
+          r: signature.r.toString(),
+          s: signature.s.toString(),
+          recovery: signature.recovery,
+        },
       });
       setBalance(balance);
     } catch (ex) {
+      console.error(ex);
       alert(ex.response.data.message);
     }
   }
--- client/src/Wallet.jsx
+++ client/src/Wallet.jsx
@@ -1,21 +1,30 @@
 import { secp256k1 as scep } from "ethereum-cryptography/secp256k1.js";
+import { keccak256 } from "ethereum-cryptography/keccak.js";
 import server from "./server";
 import { toHex } from "ethereum-cryptography/utils";
 
-function Wallet({ address, setAddress, balance, setBalance, privateKey, setPrivateKey }) {
+function Wallet({
+  address,
+  setAddress,
+  balance,
+  setBalance,
+  privateKey,
+  setPrivateKey,
+}) {
   async function onChange(evt) {
     const privateKey = evt.target.value;
     setPrivateKey(privateKey);
-    
+
     if (privateKey.length == 64) {
-      const publicKey = scep.getPublicKey(privateKey)
-      const address = `0x${toHex(publicKey).slice(-10)}`
+      const publicKey = scep.getPublicKey(privateKey);
+
+      const address = `0x${toHex(keccak256(publicKey.slice(1)).slice(-20))}`;
 
       const {
         data: { balance },
       } = await server.get(`balance/${address}`);
       setBalance(balance);
-      setAddress(address)
+      setAddress(address);
     } else {
       setBalance(0);
     }
@@ -27,11 +36,15 @@ function Wallet({ address, setAddress, balance, setBalance, privateKey, setPriva
 
       <label>
         Private Key
-        <input placeholder="Type your private key" value={privateKey} onChange={onChange}></input>
+        <input
+          placeholder="Type your private key"
+          value={privateKey}
+          onChange={onChange}
+        ></input>
       </label>
 
       <div className="balance">Address: {address}</div>
-      
+
       <div className="balance">Balance: {balance}</div>
     </div>
   );
